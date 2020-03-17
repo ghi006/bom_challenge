b6ab2785869598a813c81d39cf825267c420b1f3
Ghimire <ghi006@csiro.au>
2020-03-17 16:50:46 +1100

    cleaned script-BOM

--- cleaned script-BOM data.R
+++ cleaned script-BOM data.R
@@ -28,7 +28,7 @@ lowest_ave_temp <- Bom_data %>%
   filter(min_temp>=0,max_temp>=0)%>%
   mutate(min_temp=as.numeric(min_temp))%>%
   mutate(max_temp=as.numeric(max_temp)) %>%
-  mutate(temp_diff=max_temp-min_temp) %>%
+  mutate(temp_diff=max_temp-min_temp)%>%
   group_by(Month) %>% 
   summarise(average=mean(temp_diff)) 
 
@@ -36,7 +36,7 @@ view(Bom_stations)
 
 write_csv(lowest_ave_temp,"data/bom_data_diff.csv")
 
-
+Bom_stations
 #Challenge 3
 Tidy_bom_station <- Bom_stations %>% 
   gather(key = Station_number, value = amount,-info) %>% 
@@ -46,5 +46,12 @@ Tidy_bom_station <- Bom_stations %>%
   group_by(state) %>% 
   write_csv("data/Tidy_bom_station.csv")
 
-stations_meteo_merge <- full_join(Bom_data,Tidy_bom_station, by=c("Station_number"="Station_no"))
+Bom_stations %>% 
+  gather(key = Station_number, value = amount,-info) %>% 
+  spread(key = info, value = amount) %>%
+  mutate(Station_no=as.numeric(Station_number))
+
+
+stations_meteo_merge <- full_join(Bom_data,Tidy_bom_station, by=c("Station_number"="Station_no")) %>% 
+  write_csv("data/Bom_stations_merge.csv")
 
